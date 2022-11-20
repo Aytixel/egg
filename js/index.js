8d6b7d11a841ac9f85199c56d0fb40349ae2b452
Aytixel <aytixel@gmail.com>
2022-11-20 22:11:21 +0100

    Add a 3d egg

--- js/index.js
+++ js/index.js
@@ -1,58 +1,118 @@
-function debounce(callback, delay){
-    let timer
+import {
+  AmbientLight,
+  Color,
+  DirectionalLight,
+  Mesh,
+  MeshStandardMaterial,
+  PerspectiveCamera,
+  PlaneGeometry,
+  PointLight,
+  Scene,
+  WebGLRenderer,
+} from "three";
+import { OBJLoader } from "https://unpkg.com/three@0.146.0/examples/jsm/loaders/OBJLoader.js";
+import { MTLLoader } from "https://unpkg.com/three@0.146.0/examples/jsm/loaders/MTLLoader.js";
+import { OrbitControls } from "https://unpkg.com/three@0.146.0/examples/jsm/controls/OrbitControls.js";
 
-    return function(){
-        let args = arguments, context = this
+function debounce(callback, delay) {
+  let timer;
 
-        clearTimeout(timer)
+  return function () {
+    let args = arguments, context = this;
 
-        timer = setTimeout(function () {
-            callback.apply(context, args)
-        }, delay)
-    }
+    clearTimeout(timer);
+
+    timer = setTimeout(function () {
+      callback.apply(context, args);
+    }, delay);
+  };
 }
 
-document.getElementsByTagName("video")[0].playbackRate = 0.75
-        
-let code = ""
+document.getElementsByTagName("video")[0].playbackRate = 0.75;
+
+let code = "";
 const code_end = debounce(() => {
-    switch (code.toLocaleLowerCase()) {
-        case "arrowuparrowuparrowdownarrowdownarrowleftarrowrightarrowleftarrowrightbaenter":
-            location.href = "https://fr.wikipedia.org/wiki/Pastafarisme"
-            break
-        case "oeuf":
-            location.href = "https://maio-egg.tk/html/image.html"
-            break
-        case "sarkozy":
-            location.href = "https://www.audible.fr/pd/Le-Temps-des-Tempetes-Livre-Audio/B096W2DDK6"
-            break
-    }
+  switch (code.toLocaleLowerCase()) {
+    case "arrowuparrowuparrowdownarrowdownarrowleftarrowrightarrowleftarrowrightbaenter":
+      location.href = "https://fr.wikipedia.org/wiki/Pastafarisme";
+      break;
+    case "oeuf":
+      location.href = "https://maio-egg.tk/html/image.html";
+      break;
+    case "sarkozy":
+      location.href =
+        "https://www.audible.fr/pd/Le-Temps-des-Tempetes-Livre-Audio/B096W2DDK6";
+      break;
+  }
 
-    code = ""
-}, 1500)
+  code = "";
+}, 1500);
 
-window.addEventListener("keyup", e => {
-    code += e.key
-    code_end()
-})
+window.addEventListener("keyup", (e) => {
+  code += e.key;
+  code_end();
+});
 
-const canvas = document.getElementsByTagName("canvas")[0]
-const renderer = new THREE.WebGLRenderer({ canvas })
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(45, canvas.clientWidth / canvas.clientHeight, 1, 500);
+// canvas 3d egg
 
-renderer.setSize(canvas.clientWidth, canvas.clientHeight)
-camera.position.set(0, 0, 100)
-scene.add(camera)
+const canvas = document.getElementsByTagName("canvas")[0];
+const renderer = new WebGLRenderer({ canvas, antialias: true, alpha: true });
 
+renderer.shadowMap.enabled = true;
 
-const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
-const points = [];
-points.push(new THREE.Vector3(- 10, 0, 0));
-points.push(new THREE.Vector3(0, 10, 0));
-points.push(new THREE.Vector3(10, 0, 0));
+const scene = new Scene();
+const camera = new PerspectiveCamera(
+  40,
+  canvas.clientWidth / canvas.clientHeight,
+  1,
+  5000,
+);
 
-const geometry = new THREE.BufferGeometry().setFromPoints(points);
-const line = new THREE.Line(geometry, material);
-scene.add(line);
-renderer.render(scene, camera);
+camera.position.z = 10;
+
+// lighting
+
+scene.add(new AmbientLight(0xf7ade4, 1));
+
+const directional = new DirectionalLight(0xf7ade4, 0.65);
+
+directional.position.set(-1, 1.5, 2);
+directional.castShadow = true;
+
+scene.add(directional);
+
+// model
+
+let object;
+
+{
+  const material = new MeshStandardMaterial({ color: 0xffffff });
+  const obj_loader = new OBJLoader();
+
+  obj_loader.load("model/egg.obj", function (object_) {
+    object = object_;
+    object.traverse(function (child) {
+      if (child.isMesh) {
+        child.castShadow = true;
+        child.material = material;
+      }
+    });
+    object.rotation.z += 0.35;
+
+    scene.add(object);
+  });
+}
+
+// render loop
+
+function render() {
+  renderer.render(scene, camera);
+
+  if (object) {
+    object.rotation.y += 0.0025;
+  }
+
+  requestAnimationFrame(render);
+}
+
+render();
